96bd21127c0703b59eb6959763ed50df14d20f39
alke <bunpig@protonmail.com>
2024-04-28 13:21:59 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h2>Hello! We are practicing VPS deployment.</h2>
+      <p>This is our app.</p>
+      <p>Let's try to deploy this app on a VPS, as securely as po.</p>
+    </>
+  )
+}
+
+export default App
